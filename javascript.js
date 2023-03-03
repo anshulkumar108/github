9ebc104f2c96d4506ab4b9aeadd5d77d8b84c8c7
ANSHUL KUMAR <anshulme96@gmail.com>
2023-03-03 23:12:34 +0530

    sharpnertut

--- javascript.js
+++ javascript.js
@@ -4,9 +4,9 @@
 var b=document.getElementsByClassName('list-group-item');
 console.log(b)
 console.log(b[1])
-b[1].textContent= "kheer"
-b[1].style.backgroundColor ="yellow"
-b[1].style.fontWeight="bold"
+//b[1].textContent= "kheer"
+//b[1].style.backgroundColor ="yellow"
+//b[1].style.fontWeight="bold"
 
 //give error
 //b.style.backgroundColor="pink"
