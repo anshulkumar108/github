9fc3847c96c20057b7e716d29f068981cd07f0fc
ANSHUL KUMAR <anshulme96@gmail.com>
2023-03-03 20:44:02 +0530

    learn from tutuorial

--- javascript.js
+++ javascript.js
@@ -4,3 +4,13 @@
 var b=document.getElementsByClassName('list-group-item');
 console.log(b)
 console.log(b[1])
+b[1].textContent= "kheer"
+b[1].style.backgroundColor ="yellow"
+b[1].style.fontWeight="bold"
+
+//give error
+//b.style.backgroundColor="pink"
+
+for(var i=0 ;i<b.length;i++){
+    b[i].style.backgroundColor="pink"
+}
