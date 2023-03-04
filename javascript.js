498ed8ee3af5505331492e050007d475fd6bfd12
Anshul kumar <anshulme96@gmail.com>
2023-03-04 21:45:07 +0530

    QUERYSEELECTOR

--- javascript.js
+++ javascript.js
@@ -37,3 +37,24 @@ console.log(li)
 //     li[i].style.fontWeight="bold"
 //     li[i].style.backgroundColor ="yellow"
 // }
+
+//QUERYSELECTOR
+
+var input =document.querySelector('input')
+input.value="hello world"
+
+var header = document.querySelector('#main-header')
+header.style.border="solid 2px blue"
+
+var submit =document.querySelector('input[type="submit"]')
+submit.value="SEND"
+submit.style.backgroundColor="red"
+
+var item=document.querySelector('.list-group-item')
+item.style.backgroundColor="orange"
+
+var lastitem=document.querySelector('.list-group-item:last-child')
+lastitem.style.backgroundColor="purple"
+
+var seconditem=document.querySelector('.list-group-item:nth-child(2)')
+seconditem.style.backgroundColor="grey"
