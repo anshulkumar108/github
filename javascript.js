4210bfd71d842787dd56646f0582f6cf69dceb6b
Anshul kumar <anshulme96@gmail.com>
2023-03-04 07:52:18 +0530

    tutorial 1

--- javascript.js
+++ javascript.js
@@ -4,19 +4,19 @@
 var b=document.getElementsByClassName('list-group-item');
 console.log(b)
 console.log(b[1])
-//b[1].textContent= "kheer"
-//b[1].style.backgroundColor ="yellow"
-//b[1].style.fontWeight="bold"
+b[1].textContent= "kheer"
+b[1].style.backgroundColor ="yellow"
+b[1].style.fontWeight="bold"
 
 //give error
-//b.style.backgroundColor="pink"
+b.style.backgroundColor="pink"
 
-/*for(var i=0 ;i<b.length;i++){
+for(var i=0 ;i<b.length;i++){
     b[i].style.backgroundColor="pink"
-}*/
+}
 
-b[2].style.backgroundColor ="green"
+/*b[2].style.backgroundColor ="green"
 //b[1].style.fontWeight="bold"
 for(var i=0 ;i<b.length;i++){
     b[i].style.fontWeight="bold"
-}
+}*/
