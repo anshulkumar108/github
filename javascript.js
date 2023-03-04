54e7b7040e3c9020a1ef1a70f8a61e983db6554f
Anshul kumar <anshulme96@gmail.com>
2023-03-04 07:55:38 +0530

    assignment 1

--- javascript.js
+++ javascript.js
@@ -2,21 +2,21 @@
 //console.log(a)
 
 var b=document.getElementsByClassName('list-group-item');
-console.log(b)
-console.log(b[1])
-b[1].textContent= "kheer"
-b[1].style.backgroundColor ="yellow"
-b[1].style.fontWeight="bold"
+//console.log(b)
+//console.log(b[1])
+//b[1].textContent= "kheer"
+//b[1].style.backgroundColor ="yellow"
+//b[1].style.fontWeight="bold"
 
 //give error
-b.style.backgroundColor="pink"
+//b.style.backgroundColor="pink"
 
-for(var i=0 ;i<b.length;i++){
-    b[i].style.backgroundColor="pink"
-}
+//for(var i=0 ;i<b.length;i++){
+    //b[i].style.backgroundColor="pink"
+//}
 
-/*b[2].style.backgroundColor ="green"
-//b[1].style.fontWeight="bold"
+b[2].style.backgroundColor ="green"
+b[1].style.fontWeight="bold"
 for(var i=0 ;i<b.length;i++){
     b[i].style.fontWeight="bold"
-}*/
+}
