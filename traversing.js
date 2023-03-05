86a11aa2f27a333359575966ab5d2b55fe6382e3
Anshul kumar <anshulme96@gmail.com>
2023-03-05 05:44:25 +0530

    PARENT ELEMENT

--- traversing.js
+++ traversing.js
@@ -1,8 +1,17 @@
 var itemlist=document.querySelector('#items')
 //PARENT NODE
-console.log(itemlist.parentNode)
-itemlist.parentNode.style.backgroundColor="grey"
-console.log(itemlist.parentNode.parentNode)
-itemlist.parentNode.parentNode.style.backgroundColor="lightblue"
-console.log(itemlist.parentNode.parentNode.parentNode)
-itemlist.parentNode.parentNode.parentNode.style.backgroundColor="pink"
+// console.log(itemlist.parentNode)
+// itemlist.parentNode.style.backgroundColor="grey"
+// console.log(itemlist.parentNode.parentNode)
+// itemlist.parentNode.parentNode.style.backgroundColor="lightblue"
+// console.log(itemlist.parentNode.parentNode.parentNode)
+// itemlist.parentNode.parentNode.parentNode.style.backgroundColor="pink"
+
+//PARENT ELEMENT
+console.log(itemlist.parentElement)
+itemlist.parentElement.style.backgroundColor="grey"
+console.log(itemlist.parentElement.parentElement)
+itemlist.parentElement.parentElement.style.backgroundColor="lightblue"
+
+
+
