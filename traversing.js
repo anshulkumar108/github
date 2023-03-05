76e784573f9970f55ae608bb75ef183acb8dd90b
Anshul kumar <anshulme96@gmail.com>
2023-03-05 14:54:52 +0530

    Sibling

--- traversing.js
+++ traversing.js
@@ -8,33 +8,42 @@ var itemlist=document.querySelector('#items')
 // itemlist.parentNode.parentNode.parentNode.style.backgroundColor="pink"
 
 //PARENT ELEMENT
-console.log(itemlist.parentElement)
-itemlist.parentElement.style.backgroundColor="grey"
-console.log(itemlist.parentElement.parentElement)
-itemlist.parentElement.parentElement.style.backgroundColor="lightblue"
-console.log(itemlist.parentElement.parentElement.parentElement)
-itemlist.parentElement.parentElement.parentElement.style.backgroundColor="pink"
+// console.log(itemlist.parentElement)
+// itemlist.parentElement.style.backgroundColor="grey"
+// console.log(itemlist.parentElement.parentElement)
+// itemlist.parentElement.parentElement.style.backgroundColor="lightblue"
+// console.log(itemlist.parentElement.parentElement.parentElement)
+// itemlist.parentElement.parentElement.parentElement.style.backgroundColor="pink"
 
 
 
-//CHILD NODE : this take empty spaces also.
-console.log(itemlist.childNodes)
+// //CHILD NODE : this take empty spaces also.
+// console.log(itemlist.childNodes)
 
-//childern will take empty space .it gives only itemlist.
-console.log(itemlist.children)
-itemlist.children[1].style.backgroundColor="orange"
+// //childern will take empty space .it gives only itemlist.
+// console.log(itemlist.children)
+// itemlist.children[1].style.backgroundColor="orange"
 
-//FIRST CHILD NODE : this take empty spaces also.
-console.log(itemlist.firstChild)
+// //FIRST CHILD NODE : this take empty spaces also.
+// console.log(itemlist.firstChild)
 
-//childern will take empty space .it gives only itemlist.
-console.log(itemlist.firstElementChild)
-itemlist.firstElementChild.style.backgroundColor="magenta"
+// //childern will take empty space .it gives only itemlist.
+// console.log(itemlist.firstElementChild)
+// itemlist.firstElementChild.style.backgroundColor="magenta"
 
-//FIRST CHILD NODE : this take empty spaces also.
-console.log(itemlist.lastChild)
+// //FIRST CHILD NODE : this take empty spaces also.
+// console.log(itemlist.lastChild)
 
-//childern will take empty space .it gives only itemlist.
-console.log(itemlist.lastElementChild)
-itemlist.lastElementChild.style.backgroundColor="purple"
+// //childern will take empty space .it gives only itemlist.
+// console.log(itemlist.lastElementChild)
+// itemlist.lastElementChild.style.backgroundColor="purple"
 
+//nextSibling
+console.log(itemlist.nextSibling)
+//nextElementSibling
+console.log(itemlist.nextElementSibling)
+
+//previousSibling
+console.log(itemlist.previousSibling)
+//nextElementSibling
+console.log(itemlist.previousElementSibling)
