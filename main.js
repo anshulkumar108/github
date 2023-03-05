2f14defa4be15dc890c748e604ab886030f1421b
Anshul kumar <anshulme96@gmail.com>
2023-03-05 22:09:19 +0530

    shifting edit btn to right

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 var form = document.getElementById('addForm')
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
 //form submit event
 form.addEventListener('submit', addItem)
 
 // Delete event
-itemList.addEventListener('click',removeItem)
+itemList.addEventListener('click', removeItem)
 
+// //adding edit button
+// itemList.addEventListener('click', editBtn)
 
-
+// Filter event
+filter.addEventListener('keyup', filterItems);
 // Add item
 function addItem(e) {
     e.preventDefault();
@@ -43,35 +47,52 @@ function addItem(e) {
     //append li to list
     itemList.appendChild(li)
 
-//adding edit button
-itemList.addEventListener('click', editBtn)
-
-//create delete btn
-var editbtn = document.createElement('button')
 
 
-//add classes to delete btn
-editbtn.className = "edit"
+}
 
-li.appendChild(editbtn)
-
-itemList.appendChild(li)
-
-   
-}  
+// //create delete btn
+// var editbtn = document.createElement('button')
 
 
+// //add classes to delete btn
+// editbtn.className = "edit"
 
-    // remove item function
-function removeItem(e){
+// li.appendChild(editbtn)
+
+// itemList.appendChild(li
+
+//     // remove item function
+function removeItem(e) {
     e.preventDefault();
     //console.log(1);
 
-    if(e.target.classList.contains('delete')){
-        if(confirm('Are You Sure?')){
-          var li = e.target.parentElement;
-          itemList.removeChild(li);
-        }
-      }
 
+    if (e.target.classList.contains('delete')) {
+        if (confirm('Are You Sure?')) {
+            var li = e.target.parentElement;
+            itemList.removeChild(li);
+        }
+
+    }
+
+}
+
+// Filter Items
+function filterItems(e) {
+    // convert text to lowercase
+    var text = e.target.value.toLowerCase()
+    //console.log(text)
+    // Get lis
+    var items = itemList.getElementsByTagName('li');
+    console.log(items)
+    // Convert to an array
+    // Array.from(items).forEach(function (item) {
+    //     var itemName = item.firstChild.textContent;
+    //     if (itemName.toLowerCase().indexOf(text) != -1) {
+    //         item.style.display = 'block';
+    //     } else {
+    //         item.style.display = 'none';
+    //     }
+    // });
 }
