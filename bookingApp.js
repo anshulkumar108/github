b084269b46eedb49ddefb12f582720e3bff420ab
Anshul kumar <anshulme96@gmail.com>
2023-03-08 18:10:26 +0530

    adding data in local storage and creating list which can delete selected items

--- bookingApp.js
+++ bookingApp.js
@@ -1,4 +1,3 @@
-
 function myfunc(event) {
     event.preventDefault();
      var  inputName=document.getElementById('name').value;
@@ -7,12 +6,25 @@ function myfunc(event) {
         inputName,
         inputEmail
     };
- 
+
     let myobj_serialized=JSON.stringify(mydata)
     localStorage.setItem("data", myobj_serialized)
 
     const userList=document.querySelector('#users')
     const list=document.createElement('li')  
-         list.appendChild(document.createTextNode( inputName + ' ' + inputEmail ))
-         userList.appendChild(list)
+     list.appendChild(document.createTextNode( inputName + ' ' + inputEmail ))
+     userList.appendChild(list)
+
+
+     const myspan = document.createElement('button')
+        myspan.className = 'btn float-right'
+        myspan.innerHTML = 'DELETE';
+        userList.appendChild(myspan)
+
+        const close = document.querySelectorAll('button')
+         for (let i = 0; i < close.length; i++) {
+        close[i].addEventListener('click', () => {
+            close[i].parentElement.style.display = 'none'
+        })
+    }
 }
